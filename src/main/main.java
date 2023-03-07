9885a7a546ec9602644cc0509815f70908bc7cd4
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-03-07 13:11:11 +0100

    ran the formatter

--- src/main/main.java
+++ src/main/main.java
@@ -1,8 +1,8 @@
 package main;
 
 public class main {
-public static void main (String[]args) {
-
+    public static void Main(String[] args) {
+        System.out.println("Hello World");
     }
 }
 
