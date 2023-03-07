1719adbf60d694d6c4e09ddff2372abdbf2cdeeb
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-03-07 14:32:48 +0100

    new message

--- src/main/main.java
+++ src/main/main.java
@@ -3,7 +3,7 @@ package main;
 public class main {
 public static void main (String[]args) {
     System.out.println("Devon");
-    System.out.println("new");
+    System.out.println("new ");
     }
 }
 
